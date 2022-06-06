c0389ec7dd67c0fcb9b1b0a18f917731f3c67a5c
Patrick Collings <patrickcollingss@gmail.com>
2022-06-06 14:13:59 +0100

    change navbar items to icons

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -3,20 +3,21 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import logo from "../../assets/logo.png";
+import { RestartAlt, Done } from "@mui/icons-material/";
+
 
 export default function NavBar(props) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar sx={{ justifyContent: "space-between" }}>
-          <img src={logo} style={{height: '64px', width: '64px'}}/>
-          <div style={{display: 'flex'}}>
-            <Button color="inherit" onClick={props.openClick}>
-              Finish
-            </Button>
-            <Button color="inherit" onClick={props.resetClick}>
-              Reset
-            </Button>
+          <img src={logo} style={{ height: "64px", width: "64px" }} />
+          <div style={{ display: "flex" }}>
+            <RestartAlt
+              onClick={props.resetClick}
+              style={{ marginRight: "1rem", cursor: "pointer" }}
+            />
+            <Done onClick={props.openClick} style={{ cursor: "pointer" }} />
           </div>
         </Toolbar>
       </AppBar>
